7ad011ffe8b1b6a505e68a0ebf4cab3e7bf598e5
priyavpd <vpriyacse2@gmail.com>
2016-07-23 12:20:33 +0530

    Create Reverse.java

---
+++ Reverse.java
@@ -0,0 +1,13 @@
+
+public class Reverse
+{
+public static void main(String[] args)
+{
+	String string="priya";
+    String reverse = new StringBuffer(string).
+    reverse().toString();
+    System.out.println("\nString before reverse: "+string);
+    System.out.println("String after reverse: "+reverse);;
+}
+}
+
